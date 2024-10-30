53afa9b8ab6d90a0ce5dd1bbd3bc3abae1a5af55
galidekel <galidekel.mail@gmail.com>
2024-10-30 16:32:20 +0200

    save polygons in longlat!!!!

--- polygs.py
+++ polygs.py
@@ -1,12 +1,30 @@
 import os
-import geopandas as gpd
+
 from get_intf_info import *
 from shapely.geometry import Polygon
+
+import numpy as np
+from affine import Affine
 import matplotlib.pyplot as plt
 
+import rasterio
+from rasterio.features import shapes
+
+import rasterio.features
+from shapely.geometry import shape, polygon, box
+
+import geopandas as gpd
+
+
+def mask_array_to_polygons(mask_array):
+    transform = Affine.identity()    # Generate polygons from the mask array
+    polygons = shapes(mask_array, transform=transform)
+    shapely_polygons = [shape(geom) for geom, value in polygons if value == 1]
+    polygons_gpd = gpd.GeoDataFrame(geometry=shapely_polygons,crs="EPSG:4326")
+    return polygons_gpd
 
 def plg_indx2longlat(polyg_gdf, intf_coords):
-    x4000, y0, dx, dy = intf_coords[5], intf_coords[1], intf_coords[2], intf_coords[3]
+    x4000, y0, dx, dy = intf_coords[6], intf_coords[1], intf_coords[2], intf_coords[3]
     polyg_list = polyg_gdf['geometry'].tolist()
     polyg_longlat = []
     for polyg in polyg_list:
@@ -16,24 +34,35 @@ def plg_indx2longlat(polyg_gdf, intf_coords):
 
     polyg_longlat_gdf = gpd.GeoDataFrame(geometry=polyg_longlat, crs='EPSG:4326')
     return polyg_longlat_gdf
+if __name__ == '__main__':
+    plt.rcParams['backend'] = 'Qt5Agg'
+
+    directory_path = 'pred_outputs2/job_train_11ddiffs_rand_by_patch_2024-08-11_22h33checkpoint_epoch125/job_10_29_18h26/'
+    dir_polygs = directory_path #+ 'polygs/'
+    outpath = dir_polygs + 'polygs_longlat/'
+    os.makedirs(outpath, exist_ok=True)
+    file_list = os.listdir(dir_polygs)
+    for file in file_list:
+        if file.endswith('.shp'):
+            polyg_gdf = gpd.read_file(dir_polygs + file)
+            intf = file[:17]
+            intf_coords = get_intf_coords(intf)
+            polyg_longlat = plg_indx2longlat(polyg_gdf,intf_coords)
+            polyg_longlat.to_file(outpath+intf+'_predicted_polygons_longlat.shp')
 
 
-# List all files and directories in the specified path
-directory_path = 'pred_outputs/job_train_11days_preset_by_intf_20_05_13h45_2024-05-24_13h35checkpoint_epoch60/polygs/'
-outpath = directory_path + 'polygs_longlat/'
-os.makedirs(outpath, exist_ok=True)
-file_list = os.listdir(directory_path)
-for file in file_list:
-    if file.endswith('.shp'):
-        polyg_gdf = gpd.read_file(directory_path + file)
-        intf = file[:17]
-        intf_coords = get_intf_coords(intf)
-        polyg_longlat = plg_indx2longlat(polyg_gdf,intf_coords)
-        polyg_longlat.to_file(outpath+intf+'_predicted_polygons_longlat.shp')
+            polygs_longlat_gdf = gpd.read_file(outpath+intf+'_predicted_polygons_longlat.shp')
+            image = np.load( directory_path + intf +'_image.npy', allow_pickle=True)
+            fig, ax1 = plt.subplots(1, 1)
+            x0, y0, dx, dy, ncells, nlines, x4000, x8500, intf_lidar_mask,num_nz = get_intf_coords(intf)
+
+            extent = [x4000, x8500, y0 - dy * nlines, y0]
 
 
-        polygs_longlat_gdf = gpd.read_file(outpath+intf+'_predicted_polygons_longlat.shp')
-        polygs_longlat_gdf.plot()
+
+            #ax1.imshow(image,cmap='jet')
+            polyg_longlat.plot(ax=ax1, facecolor='none', edgecolor='black')
+
         plt.show()
 
 
--- predict_full_intf.py
+++ predict_full_intf.py
@@ -1,41 +1,14 @@
 import sys
 import pickle
-
-import torch
-import torch.nn.functional as F
-from tqdm import tqdm
-
-from dice_score import multiclass_dice_coeff, dice_coeff
-
-import numpy as np
-from pathlib import Path
-
-from affine import Affine
-import matplotlib.pyplot as plt
-
-import rasterio
-from rasterio.features import shapes
-
 import rasterio.features
-from shapely.geometry import shape, polygon, box
-
-import geopandas as gpd
 from unet import *
-
-from os import listdir
-
-import json
-
+from polygs import *
 import os
 import rasterio
 from rasterio.features import rasterize
 import logging
 from datetime import datetime
 from get_intf_info import *
-from shapely.affinity import affine_transform
-from shapely.geometry import Polygon
-
-
 
 def str2bool(arg):
     if arg.lower() == 'true':
@@ -45,7 +18,7 @@ def str2bool(arg):
     return arg
 
 def reconstruct_intf(data,mask,intf_coords,net,add_lidar_mask = True,plot = False):
-    x0, y0, dx, dy, ncells, nlines, x4000, x8500, intf_lidar_mask = intf_coords
+    x0, y0, dx, dy, ncells, nlines, x4000, x8500, intf_lidar_mask,num_nonz_p = intf_coords
     data = (data + np.pi) / (2 * np.pi)
     assert data.ndim == 4 and mask.ndim == 4, "number of input dims should be 4 got data: {} mask: {} instead".format(
         data.ndim, mask.ndim)
@@ -148,8 +121,6 @@ def reconstruct_intf(data,mask,intf_coords,net,add_lidar_mask = True,plot = Fals
 
     return reconstructed_intf,reconstructed_mask,reconstructed_pred
 
-
-
 def get_pred_args():
     import argparse
     parser = argparse.ArgumentParser(description='Prepare patches of intrfrgrm data')
@@ -178,7 +149,11 @@ def get_pred_args():
 
     return parser.parse_args()
 
+
+
 if __name__ == '__main__':
+    plt.rcParams['backend'] = 'Qt5Agg'
+
     logging.basicConfig(level=logging.INFO)  # Set the logging level (e.g., INFO)
     is_running_locally = os.environ.get('LOCAL_ENVIRONMENT', False)
     now = datetime.now().strftime("%m_%d_%Hh%M")
@@ -239,6 +214,10 @@ if __name__ == '__main__':
     data_dir = args.input_patch_dir + 'data_patches_H' + str(patch_H) + '_W' + str(patch_W)+'_strpp{}'.format(args.data_stride) + ('_11days' if args.eleven_days_diff else '_all')
     mask_dir = args.input_patch_dir + 'mask_patches_H' + str(patch_H) + '_W' + str(patch_W) + '_strpp{}'.format(args.data_stride) + ('_11days' if args.eleven_days_diff else '_all')
 
+
+    data_dir = 'data_patches_H200_W100_strpp2_11days_22_23'
+    mask_dir = 'mask_patches_H200_W100_strpp2_11days_22_23'
+
     for intf in intf_list:
 
         intfs_coords= get_intf_coords(intf)
@@ -250,37 +229,21 @@ if __name__ == '__main__':
         mask = np.load(mask_path)
         reconstructed_intf,reconstructed_mask,reconstructed_pred = reconstruct_intf(data,mask,intfs_coords,net,args.data_stride,args.plot)
 
-        def mask_array_to_polygons(mask_array, north, west, pixel_size):
-            polygons = []
-            height, width = mask_array.shape
-            for y in range(height - 1):
-                for x in range(width - 1):
-                    if mask_array[y, x] == 1:
-                        # Calculate vertices in latlon coordinates
-                        lon1 = west + x * pixel_size
-                        lon2 = west + (x + 1) * pixel_size
-                        lat1 = north - y * pixel_size
-                        lat2 = north - (y + 1) * pixel_size
-                        # Define polygon vertices in lon-lat order
-                        vertices = [(lon1, lat1), (lon2, lat1), (lon2, lat2), (lon1, lat2)]
-                        # Create polygon and append to list
-                        polygons.append(Polygon(vertices))
-            return polygons
 
-        x0, y0, dx, dy, ncells, nlines, x4000, x8500, intf_lidar_mask = intfs_coords
+
+        x0, y0, dx, dy, ncells, nlines, x4000, x8500, intf_lidar_mask,num_nz = intfs_coords
 
         # Convert mask array to polygons with latlon coordinates
-        polygons = mask_array_to_polygons(reconstructed_pred, y0, x4000, dx)
+        polygons = plg_indx2longlat(mask_array_to_polygons(reconstructed_pred),intfs_coords)
 
 
-        polygons_gpd = gpd.GeoDataFrame(geometry=polygons, crs='EPSG:4326')  # Adjust CRS as necessary
+
+        list_of_polygons = polygons.geometry.tolist()
 
 
-        polygons_gpd.plot()
-        plt.show()
         prefix = output_path  + intf
         out_polyg_path = prefix + '_predicted_polyogns.shp'
-        polygons_gpd.to_file(out_polyg_path)
+        polygons.to_file(out_polyg_path)
 
 
         np.save(prefix +'_pred', reconstructed_pred)
@@ -289,36 +252,36 @@ if __name__ == '__main__':
 
 
         if args.plot:
-            fig, (ax1, ax2,ax3) = plt.subplots(1, 3, figsize=(10,5))
-            extent = [x4000, x8500, y0 - dy * nlines, y0]
+            fig, (ax1, ax2,ax3) = plt.subplots(1, 3, figsize=(10,5),sharex=True,sharey=True)
+            extent = [x4000, x4000+dx*reconstructed_intf.shape[1], y0 - dy * reconstructed_intf.shape[0], y0]
 
-            # ax1.imshow(full_intf_data)
-            # ax1.set_title('Orig Image')
 
             ax1.imshow(reconstructed_intf,extent=extent)
             ax1.set_title('Reconstructed Image')
-            polygons_gpd.plot(ax=ax1, facecolor='none', edgecolor='white')
+            for p in list_of_polygons:
+                x,y = p.exterior.xy
+                ax1.plot(x,y)
 
             ax2.imshow(reconstructed_mask,extent=extent)
             ax2.set_title('Reconstructed True mask')
 
             ax3.imshow(reconstructed_pred,extent=extent)
             ax3.set_title('pred mask')
-            def on_xlims_change(axes):
-                for ax in (ax1, ax2, ax3):
-                    if ax != axes:
-                        ax.set_xlim(axes.get_xlim())
-
-
-            def on_ylims_change(axes):
-                for ax in (ax1, ax2, ax3):
-                    if ax != axes:
-                        ax.set_ylim(axes.get_ylim())
-
-
-            # Connect the events
-            ax1.callbacks.connect('xlim_changed', on_xlims_change)
-            ax1.callbacks.connect('ylim_changed', on_ylims_change)
+            # def on_xlims_change(axes):
+            #     for ax in (ax1, ax2, ax3):
+            #         if ax != axes:
+            #             ax.set_xlim(axes.get_xlim())
+            #
+            #
+            # def on_ylims_change(axes):
+            #     for ax in (ax1, ax2, ax3):
+            #         if ax != axes:
+            #             ax.set_ylim(axes.get_ylim())
+            #
+            #
+            # # Connect the events
+            # ax1.callbacks.connect('xlim_changed', on_xlims_change)
+            # ax1.callbacks.connect('ylim_changed', on_ylims_change)
 
             # Show the plot
             plt.show()
